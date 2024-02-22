af973b923276b7bbc1113735ae7bd2cae37b809a
Syamsul Arifin <arifsaqa@gmail.com>
2024-02-22 23:30:09 +0700

    url e lapo seh :(  /carts/:productId to /carts

--- app/cart/router.js
+++ app/cart/router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const cartController = require("./controller");
 const { police_check } = require("../../middlewares");
 
-router.put("/carts/:productId", police_check("update", "Carts"), cartController.update);
+router.put("/carts", police_check("update", "Carts"), cartController.update);
 router.delete("/carts", cartController.deleteCartItemById);
 router.get("/carts", police_check("read", "Carts"), cartController.index);
 
